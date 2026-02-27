34122f3890b51ad0863bdde0b0ee5636d6575f7e
Kunal Verma <kunalverma3229@gmail.com>
2026-02-27 22:45:12 +0530

    Update faah to v1.0.2

--- Formula/faah.rb
+++ Formula/faah.rb
@@ -1,9 +1,9 @@
 class Faah < Formula
   desc "Terminal error scream CLI"
   homepage "https://github.com/kunal3229/faah"
-  url "https://github.com/kunal3229/faah/archive/refs/tags/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "bac8ce53b0bc9cbd5e288ce48d3239b558cc5851341bdfb66b02cc45bcc45fe1"
+  url "https://github.com/kunal3229/faah/archive/refs/tags/v1.0.2.tar.gz" 
+  version "1.0.2"
+  sha256 "a59df74c57433252c474d6f08cfb8a72878b2cb5028408a3937f3e3eebb3249b"
 
   def install
     bin.install "faah"
